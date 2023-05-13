27ec3ab57246d46af5aa7ca8275ccacce407ebcc
beketovekb <beketovekb@gmail.com>
2023-05-13 22:59:26 +0600

    jhgf

--- vacancy.php
+++ vacancy.php
@@ -1,3 +1,50 @@
+<?php
+if (isset ($_POST['contactFF'])) {
+  $to = "beketovekb@gmail.com";
+  $from = 'beketovekb@gmail.com';
+  $subject = "Заполнена контактная форма с ".$_SERVER['HTTP_REFERER'];
+  $message = "Имя: ".$_POST['name']."\nEmail: ".$from."\nIP: ".$_SERVER['REMOTE_ADDR']."\nСообщение: ".$_POST['list'];
+  $boundary = md5(date('r', time()));
+  $filesize = '';
+  $headers = "MIME-Version: 1.0\r\n";
+  $headers .= "From: " . $from . "\r\n";
+  $headers .= "Reply-To: " . $from . "\r\n";
+  $headers .= "Content-Type: multipart/mixed; boundary=\"$boundary\"\r\n";
+  $message="
+Content-Type: multipart/mixed; boundary=\"$boundary\"
+
+--$boundary
+Content-Type: text/plain; charset=\"utf-8\"
+Content-Transfer-Encoding: 7bit
+
+$message";
+  for($i=0;$i<count($_FILES['chooseFile']['name']);$i++) {
+      if(is_uploaded_file($_FILES['chooseFile']['tmp_name'][$i])) {
+         $attachment = chunk_split(base64_encode(file_get_contents($_FILES['chooseFile']['tmp_name'][$i])));
+         $filename = $_FILES['chooseFile']['name'][$i];
+         $filetype = $_FILES['chooseFile']['type'][$i];
+         $filesize += $_FILES['chooseFile']['size'][$i];
+         $message.="
+
+--$boundary
+Content-Type: \"$filetype\"; name=\"$filename\"
+Content-Transfer-Encoding: base64
+Content-Disposition: attachment; filename=\"$filename\"
+
+$attachment";
+     }
+   }
+   $message.="
+--$boundary--";
+
+  if ($filesize < 10000000) {
+    mail($to, $subject, $message, $headers);
+    $output = '<script>alert("Ваше сообщение получено, спасибо!");</script>';
+  } else {
+    $output = '<script>alert("Извините, письмо не отправлено. Размер всех файлов превышает 10 МБ.");</script>';
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -507,6 +554,7 @@
                 </div>
             </div>
         </div>
+        <?php echo $output; ?>
         <div class="form_vacancy row">
             <div class="container">
                 <div class="row">
